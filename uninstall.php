a57a0e9a00df32dc1b9cc7347e7b316fc1f58680
Matthias Bissinger <matthias.bissinger@fau.de>
2020-06-13 06:14:06 +0200

    Fix deletion of options in uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -3,7 +3,16 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
-foreach (array('wp_nasaads_query_importer-token') as $option) {
+$wp_nasaads_query_importer_options = array(
+    'wp_nasaads_query_importer-token',
+    'wp_nasaads_query_importer-token',
+    'wp_nasaads_query_importer-template',
+    'wp_nasaads_query_importer-template_start',
+    'wp_nasaads_query_importer-template_stop',
+    'wp_nasaads_query_importer-numrecords',
+    'wp_nasaads_query_importer-empty_list'
+);
+foreach ($wp_nasaads_query_importer_options as $option) {
     delete_option($option);
     delete_site_option($option);
 }
